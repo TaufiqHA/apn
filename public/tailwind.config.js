029516d20f1297c703b2251c0ff07fb6dade3c6a
TaufiqHA <htaufiq225@gmail.com>
2022-08-24 21:55:43 +0700

    update tailwindcss

--- public/tailwind.config.js
+++ public/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.{html,js}'],
+  content: ['./../app/views/**/*.{php, html}'],
   theme: {
     extend: {
       colors:
