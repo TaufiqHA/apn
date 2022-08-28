76c5698ad5719e7d465d37907eac247dced2faa1
TaufiqHA <htaufiq225@gmail.com>
2022-08-28 16:11:20 +0700

    update config

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,6 @@
-<?php 
+<?php
 
-<<<<<<< HEAD
 const BASEURL = 'http://localhost/apn';
-=======
-const BASEURL = 'http://localhost:8080/apn';
->>>>>>> d0387433a57d0d31426424d8ac3f384c8c04c979
 
 //database;
 
