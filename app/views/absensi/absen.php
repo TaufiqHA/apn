c9558a64c7a296043b577b1853568aadddb4a4d4
TaufiqHA <htaufiq225@gmail.com>
2022-08-29 22:53:57 +0700

    memperbaiki masalah pada halaman absen

--- app/views/absensi/absen.php
+++ app/views/absensi/absen.php
@@ -8,10 +8,7 @@
                     <h2 class="font-semibold text-base text-secondary lg:text-lg uppercase"><?php echo $data['kelas'] ?> </h2>
                 </div>
                 <div class="w-full self-center px-4 mt-20 md:w-1/2 lg:w-1/2 flex flex-col items-center">
-<<<<<<< HEAD
-                    <form action="#" method="post">
-=======
-                    <form action="<?php echo BASEURL; ?>/public/absensi/updateAbsen/<?php echo $data['kelas'] ?>" method="post">
+                <form action="<?php echo BASEURL; ?>/public/absensi/updateAbsen/<?php echo $data['kelas'] ?>" method="post">
                     <label for="absen">Pertemuan ke:</label>
                     <select name="absen" id="absen" class="mb-5 bg-transparent">
                         <option value="p1">1</option>
@@ -41,16 +38,11 @@
                         <option value="pts">PTS</option>
                         <option value="pas">PAS</option>
                     </select>
->>>>>>> ea6356d3015bf2a71f7d883af02bd5ce411b45b8
                     <?php if (!empty($data['siswa'])) : ?>
                         <?php foreach($data['siswa'] as $siswa) : ?>
                             <input type="hidden" name="id<?php echo $siswa['id'] ?>" value="<?php echo $siswa['id'] ?>">
                             <a href="#">
-<<<<<<< HEAD
                                 <input type="hidden" name="id" value="<?php echo $siswa['id'] ?>">
-                                 <div class="w-[300px] h-[53px] rounded-full border-[0.5px] border-black px-2 flex items-center relative mb-7 shadow-xl">
-                                    <span class="float-left pl-2 font-bold text-sm text-slate-600 uppercase"><?php echo $siswa['name'] ?> </span>
-=======
                                  <div class="w-[300px] h-[53px] rounded-full border-[0.5px] border-black px-2 relative mb-7 shadow-xl">
                                     <span class="float-left pl-2 font-bold text-sm text-slate-600 uppercase translate-y-1/2 mt-2"><?php echo $siswa['nama'] ?></span>
                                     <select name="nilai<?php echo $siswa['id'] ?>" class="float-right translate-y-1/2 bg-transparent">
@@ -59,7 +51,6 @@
                                         <option value="S">S</option>
                                         <option value="I">I</option>
                                     </select>
->>>>>>> ea6356d3015bf2a71f7d883af02bd5ce411b45b8
                                 </div>
                             </a>
                         <?php endforeach; ?>
@@ -70,11 +61,7 @@
                     <div class="flex justify-center">
                         <button class="font-bold text-white flex justify-center items-center mt-20 text-center w-[246px] h-[47px] bg-teal-500 rounded-full shadow-lg shadow-slate-300 hover:scale-110 transition duration-300" type="submit">SIMPAN</button>
                     </div>
-<<<<<<< HEAD
-                    </form>
-=======
                 </form>
->>>>>>> ea6356d3015bf2a71f7d883af02bd5ce411b45b8
                 </div>
              </div>
         </div>
