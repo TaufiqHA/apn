6d7aa79119e400a8a31ab582400c413636751a72
TaufiqHA <htaufiq225@gmail.com>
2022-08-29 15:09:27 +0700

    update halaman absen

--- app/views/absensi/absen.php
+++ app/views/absensi/absen.php
@@ -8,11 +8,14 @@
                     <h2 class="font-semibold text-base text-secondary lg:text-lg uppercase"><?php echo $data['kelas'] ?> </h2>
                 </div>
                 <div class="w-full self-center px-4 mt-20 md:w-1/2 lg:w-1/2 flex flex-col items-center">
+                    <form action="#" method="post">
                     <?php if (!empty($data['siswa'])) : ?>
                         <?php foreach($data['siswa'] as $siswa) : ?>
+                            <input type="hidden" name="id" value="<?php echo $siswa['id'] ?>">
                             <a href="#">
                                  <div class="w-[300px] h-[53px] rounded-full border-[0.5px] border-black px-2 flex items-center relative mb-7 shadow-xl">
-                                    <span class="float-left pl-2 font-bold text-sm text-slate-600 uppercase"><?php echo $siswa['name'] ?> </span>
+                                    <span class="float-left pl-2 font-bold text-sm text-slate-600 uppercase"><?php echo $siswa['name'] ?></span>
+                                    <input type="text" name="nilai" class="w-[35px] h-[35px] rounded-full absolute right-4 border border-slate-500 outline-none text-center">
                                 </div>
                             </a>
                         <?php endforeach; ?>
@@ -20,6 +23,10 @@
                     <?php if (empty($data['siswa'])) : ?>
                         <p class="text-center text-[13px] opacity-60">" Belum ada siswa yang ditambahkan "</p>
                     <?php endif; ?>
+                    <div class="flex justify-center">
+                        <button class="font-bold text-white flex justify-center items-center mt-20 text-center w-[246px] h-[47px] bg-teal-500 rounded-full shadow-lg shadow-slate-300 hover:scale-110 transition duration-300" type="submit">TAMBAH SISWA</button>
+                    </div>
+                </form>
                 </div>
              </div>
         </div>
