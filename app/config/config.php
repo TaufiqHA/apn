888413371fd9933d6cf9f48ae461d9f72ee81fca
TaufiqHA <htaufiq225@gmail.com>
2022-09-02 22:17:59 +0700

    update config

--- app/config/config.php
+++ app/config/config.php
@@ -5,6 +5,6 @@ const BASEURL = 'http://apn.skom.id';
 //database;
 
 const HOST = 'localhost';
-const DBNAME = 'apn';
-const USER = 'root';
-const PASS = '';
+const DBNAME = 'u6hueknp_apn';
+const USER = 'u6hueknp_apn';
+const PASS = '4mn-kfz2ZVr]';
